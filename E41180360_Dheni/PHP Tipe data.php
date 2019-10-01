c1c7589c184b11b0a118b13cb16876cff0345202
Dheni46 <dhenie.satrio46@gmail.com>
2019-10-01 19:51:53 +0700

    Create PHP Tipe data.php

---
+++ E41180360_Dheni/PHP Tipe data.php
@@ -0,0 +1,20 @@
+<?php 
+$tes = "Saya Berangkat Kuliah";
+echo $tes;
+// variabel tes di atas merupakan tipe data string karena berisi text atau kalimat.
+
+// Integer
+$bilangan_pertama = 2;
+$bilangan_kedua = 16;
+echo $bilangan_pertama;
+
+// Float
+$angka = 3.14;
+
+//Bolean
+$x = false;
+$y = true;
+
+//Array
+$anggota = array("Dheni","Gulam","Idris");
+?>
