1314c88c5b3b9c1e1905a8566295f03e14438466
Gulam Mubarik Ahmad <gulammubarik77@gmail.com>
2019-10-01 19:09:29 +0700

    Create PHP Tipe data.php

---
+++ E41180359_Gulam/PHP Tipe data.php
@@ -0,0 +1,20 @@
+<?php 
+$tes = "Saya Pergi Kerja";
+echo $tes;
+// variabel tes di atas merupakan tipe data string karena berisi text atau kalimat.
+
+// Integer
+$bilangan_pertama = 14;
+$bilangan_kedua = 17;
+echo $bilangan_pertama;
+
+// Float
+$angka = 12.177;
+
+//Bolean
+$x = false;
+$y = true;
+
+//Array
+$anggota = array("Andi","Budi","Joni");
+?>
